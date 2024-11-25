018ffffa57190dd40b423f8fe11019ac8f578450
yeyo671 <leotran671@gmail.com>
2024-11-25 12:44:15 +0100

    test

--- angular/ma-to-do-web/src/app/app.module.ts
+++ angular/ma-to-do-web/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUi2uVmKzsquF22MzIUBhp4cu2JQo_hkQ",
+  authDomain: "angular-flutter-cloud.firebaseapp.com",
+  projectId: "angular-flutter-cloud",
+  storageBucket: "angular-flutter-cloud.firebasestorage.app",
+  messagingSenderId: "398506827783",
+  appId: "1:398506827783:web:2cfddeba315d49f435caa2"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodolistComponent
+    TodolistComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/ma-to-do-web/src/app/todolist/todolist.component.ts
+++ angular/ma-to-do-web/src/app/todolist/todolist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-todolist',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./todolist.component.scss']
 })
 export class TodolistComponent {
+  newTodo: string = '';
 
+  constructor(private firestore: AngularFirestore) {}
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.firestore.collection('todos').add({ task: this.newTodo, completed: false })
+        .then(() => {
+          console.log('Todo added successfully');
+          this.newTodo = '';
+        })
+        .catch(error => {
+          console.error('Error adding todo: ', error);
+        });
+    }
+  }
 }
